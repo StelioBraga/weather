3305440e676cb71a6c40285cfe6ab1993f9035d9
steliobraga <Steliobraga13@gmail.com>
2021-12-23 21:34:06 +0200

    add targetApi

--- app/src/main/java/com/mz/weather/Api/WeatherAPIClient.java
+++ app/src/main/java/com/mz/weather/Api/WeatherAPIClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class WeatherAPIClient {
 
     public static Retrofit getRetrofit() {
-        String API_BASE_URL = "http://api.openweathermap.org";
+        String API_BASE_URL = "https://api.openweathermap.org";
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(API_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/mz/weather/utils/GetWeatherIcon.java
+++ app/src/main/java/com/mz/weather/utils/GetWeatherIcon.java
@@ -6,7 +6,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 public class GetWeatherIcon {
-    String base_url_photo = "http://openweathermap.org/img/wn/";
+    String base_url_photo = "https://openweathermap.org/img/wn/";
 
     public  GetWeatherIcon(Context context, String icon, ImageView imageView){
         String url  = base_url_photo+icon+"@2x.png";
